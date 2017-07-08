a02c040744f5e13781cb5dcc6cec15c1f3f6b10a
June_Amaterasu <june.caffeine.forsmatrphone@gmail.com>
2017-07-09 02:13:25 +0900

    test now

---
+++ mysite/polls/urls.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# -coding : utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name = "index"),
+]
+
